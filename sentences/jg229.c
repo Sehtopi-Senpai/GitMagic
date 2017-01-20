a84122c3fe8593a86cf5edf2eb93647e86b09fb4
Josh Green <jg229@mail.missouri.edu>
2017-01-19 18:03:48 -0600

    Created sentence file
    I created a sentence file named jg229.c, and modified the function
    names for testing.

---
+++ sentences/jg229.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int jg229Sentence1()
+{
+  printf("Once upon a time\n");
+  return 1;
+}
+
+int jg229Sentence_FINAL()
+{
+  printf("\nThe end. \n");
+  return 1;
+}
+
+
+
